a33e1ef8634505d803ee4bae7ea7f9a50e2e08eb
Piczle <44884494+Piczle@users.noreply.github.com>
2019-10-05 16:25:17 -0400

    Add files via upload

---
+++ Golden Hack/README.md
@@ -0,0 +1,2 @@
+# ITG-Course-Website
+Course choosing visualization for Laurier Course
---
+++ Golden Hack/server/script.js
@@ -0,0 +1,22 @@
+/*
+function read_file(filename){
+  let contents = [];
+  var file = new File(["foo"], filename, {
+      type: "text/plain",
+  });
+  console.log(file);
+  return file;
+}
+
+function json_to_string(json_file){
+  let obj = JSON.parse(json_file);
+  return obj
+}
+
+function string_to_json(string){
+
+  return json_obj
+}
+*/
+
+// process.env.MONGO_URI = Null;
